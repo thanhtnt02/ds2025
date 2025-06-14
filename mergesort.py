18a21970b238941eda5f7b305ce39c1778ba86fb
Thanh Tran <thanh.trannhantam@stud.uni-goettingen.de>
2025-06-14 03:17:48 +0200

    plotting teil verbessert

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,70 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        l = 0
-        r = 0
-        i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
-
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
-
-
 import matplotlib.pyplot as plt
 
+
+def assign_value(destination, dest_index, source, source_index):
+    """Assigns a value from source to destination."""
+    destination[dest_index] = source[source_index]
+
+
+def merge_sort(lst):
+    """Sorts a list in-place using the merge sort algorithm."""
+    if len(lst) <= 1:
+        return
+
+    mid = len(lst) // 2
+    left_half = lst[:mid]
+    right_half = lst[mid:]
+
+    merge_sort(left_half)
+    merge_sort(right_half)
+
+    left_index = 0
+    right_index = 0
+    merged_index = 0
+
+    while left_index < len(left_half) and right_index < len(right_half):
+        if left_half[left_index] <= right_half[right_index]:
+            assign_value(lst, merged_index, left_half, left_index)
+            left_index += 1
+        else:
+            assign_value(lst, merged_index, right_half, right_index)
+            right_index += 1
+        merged_index += 1
+
+    while left_index < len(left_half):
+        lst[merged_index] = left_half[left_index]
+        left_index += 1
+        merged_index += 1
+
+    while right_index < len(right_half):
+        lst[merged_index] = right_half[right_index]
+        right_index += 1
+        merged_index += 1
+
+
+# Ursprüngliche Liste
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
+
+# Plot vor dem Sortieren
+plt.figure(figsize=(8, 4))
+plt.bar(range(len(my_list)), my_list, color='steelblue')
+plt.title("Vor dem Sortieren (MergeSort)")
+plt.xlabel("Index")
+plt.ylabel("Wert")
+plt.grid(True, axis='y', linestyle='--', linewidth=0.5)
+plt.tight_layout()
 plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
+
+# Sortieren
+merge_sort(my_list)
+
+# Plot nach dem Sortieren
+plt.figure(figsize=(8, 4))
+plt.bar(range(len(my_list)), my_list, color='seagreen')
+plt.title("Nach dem Sortieren (MergeSort)")
+plt.xlabel("Index")
+plt.ylabel("Wert")
+plt.grid(True, axis='y', linestyle='--', linewidth=0.5)
+plt.tight_layout()
 plt.show()
+
